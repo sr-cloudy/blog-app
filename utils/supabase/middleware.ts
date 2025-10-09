02bcd71655efe8131dd1851c45639100424dffef
krishna-patil-aot <krishna.patil@algoocean.com>
2025-10-09 19:29:00 +0530

    update env file

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -9,7 +9,7 @@ export async function updateSession(request: NextRequest) {
   // Initialize Supabase client configured to handle cookies
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll: () => request.cookies.getAll(),
